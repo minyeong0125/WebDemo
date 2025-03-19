08fad35127d406af68155479aa845708f1d58fd0
minyeong0125 <1215jane@naver.com>
2025-03-19 12:15:55 +0900

    first

---
+++ main.js
@@ -0,0 +1,35 @@
+document.getElementById('time').innerHTML = new Date().toLocaleString()
+
+function pink() {
+  document.body.style.backgroundColor = 'rgb(255, 223, 227)'
+}
+function lightblue() {
+  document.body.style.backgroundColor = 'lightblue'
+}
+function yellow() {
+  document.body.style.backgroundColor = 'lightyellow'
+}
+function reset() {
+  document.body.style.backgroundColor = 'white'
+}
+
+function showhtml() {
+  document.getElementById('fig').src = 'image/html.png'
+  document.getElementById('desc').innerHTML =
+    '<b>HTML</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다.'
+}
+function showcss() {
+  document.getElementById('fig').src = 'image/css.png'
+  document.getElementById('desc').innerHTML =
+    '<b>CSS</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다.'
+}
+function showjs() {
+  document.getElementById('fig').src = 'image/js.png'
+  document.getElementById('desc').innerHTML =
+    '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다.'
+}
+
+function hide() {
+  document.getElementById('fig').src = ''
+  document.getElementById('desc').innerHTML = ''
+}
